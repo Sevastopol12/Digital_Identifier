e7e1f889712a8b7d15d7d8fbf7d02afc8341607a
Sevastopol <141613215+Sevastopol12@users.noreply.github.com>
2024-05-01 19:30:46 +0700

    Add files via upload

---
+++ Digital_Identifier/WinForm/License.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WinForm
+{
+    partial class License
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        public void InitializeComponent()
+        {
+            license_issued_by = new Label();
+            Dprovider = new Label();
+            license_expiry_date = new Label();
+            label9 = new Label();
+            driver_license = new Label();
+            adriver_license = new Label();
+            panel1 = new Panel();
+            linkLabel1 = new LinkLabel();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // license_issued_by
+            // 
+            license_issued_by.AutoSize = true;
+            license_issued_by.Font = new Font("Verdana", 12F);
+            license_issued_by.ForeColor = SystemColors.ActiveCaptionText;
+            license_issued_by.Location = new Point(267, 201);
+            license_issued_by.Name = "license_issued_by";
+            license_issued_by.Size = new Size(92, 25);
+            license_issued_by.TabIndex = 47;
+            license_issued_by.Text = "#####";
+            // 
+            // Dprovider
+            // 
+            Dprovider.AutoSize = true;
+            Dprovider.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            Dprovider.ForeColor = SystemColors.ActiveCaptionText;
+            Dprovider.Location = new Point(40, 201);
+            Dprovider.Name = "Dprovider";
+            Dprovider.Size = new Size(116, 25);
+            Dprovider.TabIndex = 46;
+            Dprovider.Text = "Provider:";
+            // 
+            // license_expiry_date
+            // 
+            license_expiry_date.AutoSize = true;
+            license_expiry_date.Font = new Font("Verdana", 12F);
+            license_expiry_date.ForeColor = SystemColors.ActiveCaptionText;
+            license_expiry_date.Location = new Point(267, 148);
+            license_expiry_date.Name = "license_expiry_date";
+            license_expiry_date.Size = new Size(92, 25);
+            license_expiry_date.TabIndex = 45;
+            license_expiry_date.Text = "#####";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label9.ForeColor = SystemColors.ActiveCaptionText;
+            label9.Location = new Point(40, 148);
+            label9.Name = "label9";
+            label9.Size = new Size(147, 25);
+            label9.TabIndex = 44;
+            label9.Text = "Expiry date:";
+            // 
+            // driver_license
+            // 
+            driver_license.AutoSize = true;
+            driver_license.Font = new Font("Verdana", 12F);
+            driver_license.ForeColor = SystemColors.ActiveCaptionText;
+            driver_license.Location = new Point(267, 92);
+            driver_license.Name = "driver_license";
+            driver_license.Size = new Size(92, 25);
+            driver_license.TabIndex = 43;
+            driver_license.Text = "#####";
+            // 
+            // adriver_license
+            // 
+            adriver_license.AutoSize = true;
+            adriver_license.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            adriver_license.ForeColor = SystemColors.ActiveCaptionText;
+            adriver_license.Location = new Point(40, 92);
+            adriver_license.Name = "adriver_license";
+            adriver_license.Size = new Size(175, 25);
+            adriver_license.TabIndex = 42;
+            adriver_license.Text = "Driver license:";
+            // 
+            // panel1
+            // 
+            panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel1.BackColor = Color.White;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(linkLabel1);
+            panel1.Controls.Add(license_issued_by);
+            panel1.Controls.Add(Dprovider);
+            panel1.Controls.Add(license_expiry_date);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(driver_license);
+            panel1.Controls.Add(adriver_license);
+            panel1.Location = new Point(31, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(466, 258);
+            panel1.TabIndex = 48;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Cooper Black", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.Blue;
+            linkLabel1.Location = new Point(95, 23);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(240, 32);
+            linkLabel1.TabIndex = 48;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Driver's License";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(192, 255, 255);
+            panel2.Controls.Add(panel1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(536, 342);
+            panel2.TabIndex = 49;
+            // 
+            // License
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(536, 342);
+            Controls.Add(panel2);
+            Name = "License";
+            Text = "License";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        public Label license_issued_by;
+        private Label Dprovider;
+        public Label license_expiry_date;
+        private Label label9;
+        public Label driver_license;
+        private Label adriver_license;
+        private Panel panel1;
+        private Panel panel2;
+        private LinkLabel linkLabel1;
+    }
+}
---
+++ Digital_Identifier/WinForm/Login.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WinForm
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            textBox1 = new TextBox();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            a = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.None;
+            textBox1.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(314, 114);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "User-SSN";
+            textBox1.Size = new Size(335, 28);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "322454266";
+            textBox1.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.None;
+            button2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button2.BackColor = Color.Red;
+            button2.Font = new Font("Cooper Black", 9.2F);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(534, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(104, 27);
+            button2.TabIndex = 4;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.None;
+            textBox2.Cursor = Cursors.SizeAll;
+            textBox2.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(314, 170);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(335, 28);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "wH5<p|5T3<PWE*4";
+            textBox2.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.None;
+            button1.BackColor = Color.LimeGreen;
+            button1.Font = new Font("Cooper Black", 9.2F);
+            button1.ForeColor = Color.Transparent;
+            button1.Location = new Point(424, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(104, 27);
+            button1.TabIndex = 3;
+            button1.Text = "Log-In";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(75, 50);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(209, 204);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 32;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.None;
+            label3.AutoSize = true;
+            label3.Font = new Font("Cooper Black", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.DeepSkyBlue;
+            label3.Location = new Point(400, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(153, 46);
+            label3.TabIndex = 31;
+            label3.Text = "Status";
+            // 
+            // a
+            // 
+            a.Location = new Point(664, 63);
+            a.Name = "a";
+            a.Size = new Size(94, 29);
+            a.TabIndex = 33;
+            a.Text = "hi";
+            a.UseVisualStyleBackColor = true;
+            a.Click += a_Click;
+            // 
+            // Login
+            // 
+            AcceptButton = button1;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = SystemColors.ButtonHighlight;
+            CancelButton = button2;
+            ClientSize = new Size(777, 328);
+            Controls.Add(a);
+            Controls.Add(label3);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(textBox2);
+            Controls.Add(button2);
+            ForeColor = Color.CornflowerBlue;
+            Name = "Login";
+            SizeGripStyle = SizeGripStyle.Show;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button2;
+        private TextBox textBox2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Button a;
+    }
+}
